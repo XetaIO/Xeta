022d7608a248ef9e5c3db942d25507b126543d2a
Emeric <emeric.f@noze.tv>
2014-10-24 16:53:40 +0200

    Updated bootstrap and travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+use Cake\Datasource\ConnectionManager;
 
 /**
  * Test runner bootstrap.
@@ -11,3 +12,13 @@ require dirname(__DIR__) . '/config/bootstrap.php';
 
 define('TEST_APP', TESTS . 'test_app' . DS);
 define('TEST_WWW_ROOT', TEST_APP . 'webroot' . DS);
+
+ConnectionManager::config('test', [
+	'className' => 'Cake\Database\Connection',
+	'driver' => getenv('db_class'),
+	'dsn' => getenv('db_dsn'),
+	'database' => getenv('db_database'),
+	'login' => getenv('db_login'),
+	'password' => getenv('db_password'),
+	'timezone' => 'UTC'
+]);
