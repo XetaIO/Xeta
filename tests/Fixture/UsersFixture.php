a200db1ba400155acf79aaecc9b22bb8be3653ee
Emeric <emeric.f@noze.tv>
2014-10-24 16:46:42 +0200

    Updated UsersFixture

--- tests/Fixture/UsersFixture.php
+++ tests/Fixture/UsersFixture.php
@@ -5,10 +5,48 @@ use Cake\TestSuite\Fixture\TestFixture;
 
 class UsersFixture extends TestFixture {
 
-	public $import = ['table' => 'users'];
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = [
+		'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+		'username' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'password' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'email' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'first_name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'last_name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'avatar' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => '../img/avatar.png', 'comment' => '', 'precision' => null, 'fixed' => null],
+		'biography' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+		'signature' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+		'facebook' => ['type' => 'string', 'length' => 200, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'twitter' => ['type' => 'string', 'length' => 200, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'role' => ['type' => 'string', 'length' => 20, 'null' => true, 'default' => 'member', 'comment' => '', 'precision' => null, 'fixed' => null],
+		'slug' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'blog_articles_comment_count' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+		'blog_article_count' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => true, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+		'counter_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '1', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+		'register_ip' => ['type' => 'string', 'length' => 15, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'last_login_ip' => ['type' => 'string', 'length' => 15, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+		'last_login' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+		'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+		'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+		'_indexes' => [
+			'slug' => ['type' => 'index', 'columns' => ['slug'], 'length' => []],
+		],
+		'_constraints' => [
+			'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+			'username' => ['type' => 'unique', 'columns' => ['username'], 'length' => []],
+			'mail' => ['type' => 'unique', 'columns' => ['email'], 'length' => []],
+		],
+		'_options' => [
+			'engine' => 'InnoDB', 'collation' => 'utf8_general_ci'
+		],
+	];
 
 /**
- * records property
+ * Records
  *
  * @var array
  */
@@ -36,4 +74,5 @@ class UsersFixture extends TestFixture {
 			'modified' => '2014-03-17 01:18:31'
 		]
 	];
+
 }
