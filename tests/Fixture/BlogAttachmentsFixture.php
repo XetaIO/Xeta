7bb6c47bb3a6279ed1860b2560adca4225c96b97
Xeta <emeric.f@noze.tv>
2014-12-23 16:21:55 +0100

    Fix phpcs

--- tests/Fixture/BlogAttachmentsFixture.php
+++ tests/Fixture/BlogAttachmentsFixture.php
@@ -14,42 +14,42 @@ class BlogAttachmentsFixture extends TestFixture {
  *
  * @var array
  */
-    public $fields = [
-        'id' => ['type' => 'integer'],
-        'user_id' => ['type' => 'integer'],
-        'article_id' => ['type' => 'integer'],
-        'name' => ['type' => 'string', 'length' => 255],
-        'size' => ['type' => 'integer'],
-        'extension' => ['type' => 'string', 'length' => 15],
-        'url' => ['type' => 'string', 'length' => 255],
-        'download' => ['type' => 'biginteger', 'length' => 20],
-        'created' => ['type' => 'datetime'],
-        'modified' => ['type' => 'datetime'],
-        '_constraints' => [
-            'primary' => ['type' => 'primary', 'columns' => ['id']],
-        ],
-        '_options' => [
-            'engine' => 'InnoDB', 'collation' => 'utf8_general_ci'
-        ],
-    ];
+	public $fields = [
+		'id' => ['type' => 'integer'],
+		'user_id' => ['type' => 'integer'],
+		'article_id' => ['type' => 'integer'],
+		'name' => ['type' => 'string', 'length' => 255],
+		'size' => ['type' => 'integer'],
+		'extension' => ['type' => 'string', 'length' => 15],
+		'url' => ['type' => 'string', 'length' => 255],
+		'download' => ['type' => 'biginteger', 'length' => 20],
+		'created' => ['type' => 'datetime'],
+		'modified' => ['type' => 'datetime'],
+		'_constraints' => [
+			'primary' => ['type' => 'primary', 'columns' => ['id']],
+		],
+		'_options' => [
+			'engine' => 'InnoDB', 'collation' => 'utf8_general_ci'
+		]
+	];
 
 /**
  * Records
  *
  * @var array
  */
-    public $records = [
-        [
-            'id' => 1,
-            'user_id' => 1,
-            'article_id' => 1,
-            'name' => 'attachment.zip',
-            'size' => 1584,
-            'extension' => '.zip',
-            'url' => 'attachment.zip',
-            'download' => 1,
-            'created' => '2014-12-23 13:23:36',
-            'modified' => '2014-12-23 13:23:36'
-        ]
-    ];
+	public $records = [
+		[
+			'id' => 1,
+			'user_id' => 1,
+			'article_id' => 1,
+			'name' => 'attachment.zip',
+			'size' => 1584,
+			'extension' => '.zip',
+			'url' => 'attachment.zip',
+			'download' => 1,
+			'created' => '2014-12-23 13:23:36',
+			'modified' => '2014-12-23 13:23:36'
+		]
+	];
 }
