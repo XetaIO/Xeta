ae739d30ed8a2dd5a487b497c071d33d47c76a8a
Xeta <emeric.f@noze.tv>
2015-01-28 00:17:00 +0100

    Add ACL Fixtures + Testscase for AclHelper

---
+++ tests/TestCase/View/Helper/AclHelperTest.php
@@ -0,0 +1,126 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\AclHelper;
+use Cake\Network\Request;
+use Cake\Network\Session;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+class BadgesTableTest extends TestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = [
+		'app.users',
+		'app.groups',
+		'app.acos',
+		'app.aros',
+		'app.aros_acos'
+	];
+
+/**
+ * setUp
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->View = new View();
+		$this->session = new Session();
+		$this->View->request = new Request(['session' => $this->session]);
+		$this->Acl = new AclHelper($this->View);
+
+		$this->session->write([
+			'Auth' => [
+				'User' => [
+					'id' => 2,
+					'group_id' => 2
+				]
+			]
+		]);
+	}
+
+/**
+ * tearDown
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		unset($this->Acl, $this->View, $this->session);
+	}
+
+/**
+ * Test check
+ *
+ * @return void
+ */
+	public function testCheck() {
+		$result = $this->Acl->check(['controller' => 'pages', 'action' => 'home']);
+		$this->assertTrue($result);
+
+		$result = $this->Acl->check(['controller' => 'blog', 'action' => 'index']);
+		$this->assertFalse($result);
+
+		$this->session->write([
+			'Auth' => [
+				'User' => [
+					'id' => 1,
+					'group_id' => 1
+				]
+			]
+		]);
+
+		$result = $this->Acl->check(['controller' => 'pages', 'action' => 'home']);
+		$this->assertTrue($result);
+
+		$result = $this->Acl->check(['controller' => 'blog', 'action' => 'index']);
+		$this->assertTrue($result);
+	}
+
+/**
+ * Test link
+ */
+	public function testLink() {
+		$result = $this->Acl->link('test', ['controller' => 'pages', 'action' => 'home']);
+		$expected = [
+			'a' => ['href' => '/'],
+			'test',
+			'/a'
+		];
+		$this->assertHtml($expected, $result);
+
+		$result = $this->Acl->link('test', ['controller' => 'blog', 'action' => 'index']);
+		$this->assertEmpty($result);
+
+		$this->session->write([
+			'Auth' => [
+				'User' => [
+					'id' => 1,
+					'group_id' => 1
+				]
+			]
+		]);
+
+		$result = $this->Acl->link('test', ['controller' => 'pages', 'action' => 'home']);
+		$expected = [
+			'a' => ['href' => '/'],
+			'test',
+			'/a'
+		];
+		$this->assertHtml($expected, $result);
+
+		$result = $this->Acl->link('test', ['controller' => 'blog', 'action' => 'index']);
+		$expected = [
+			'a' => ['href' => '/blog'],
+			'test',
+			'/a'
+		];
+		$this->assertHtml($expected, $result);
+	}
+}
