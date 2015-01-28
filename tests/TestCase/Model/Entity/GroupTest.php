e8bf6f20c17b4a800fa7fc7ab1011797504f11c9
Xeta <emeric.f@noze.tv>
2015-01-28 02:47:24 +0100

    Add Testscase for GroupsEntity + UserEntity

---
+++ tests/TestCase/Model/Entity/GroupTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Test\TestCase\Model\Entity;
+
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+class GroupTest extends TestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = ['app.groups'];
+
+/**
+ * setUp
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$this->Groups = TableRegistry::get('Groups');
+	}
+
+/**
+ * Test parendNode
+ *
+ * @return void
+ */
+	public function testParendNode() {
+		$entity = $this->Groups->get(1);
+		$result = $entity->parentNode();
+		$this->assertNull($result);
+	}
+}
