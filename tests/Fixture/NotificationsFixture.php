bcb5413249b07cc4819acb4f61c06c102f7c9078
Xeta <emeric.f@noze.tv>
2015-05-01 01:56:50 +0200

    Added notifications fixture

---
+++ tests/Fixture/NotificationsFixture.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class NotificationsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    public $fields = [
+        'id' => ['type' => 'integer'],
+        'user_id' => ['type' => 'integer'],
+        'type' => ['type' => 'string', 'length' => 150],
+        'data' => ['type' => 'text', 'null' => true],
+        'is_read' => ['type' => 'integer', 'length' => 2],
+        'created' => ['type' => 'datetime'],
+        'modified' => ['type' => 'datetime'],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id']],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB', 'collation' => 'utf8_general_ci'
+        ],
+    ];
+}
