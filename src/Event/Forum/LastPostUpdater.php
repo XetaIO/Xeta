da9dc79a94372b46e0eebc167083c260b6247774
JcPires <djyss@live.fr>
2015-05-18 11:19:57 +0200

    Fixes #87,
    Fix deletePost() and __updateCategories() method

--- src/Event/Forum/LastPostUpdater.php
+++ src/Event/Forum/LastPostUpdater.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jc
- * Date: 12/05/2015
- * Time: 09:13
- */
 
 namespace App\Event\Forum;
 
@@ -12,88 +6,103 @@ use Cake\Event\Event;
 use Cake\Event\EventListenerInterface;
 use Cake\ORM\TableRegistry;
 
+/**
+ * Class LastPostUpdater
+ * @package App\Event\Forum
+ */
 class LastPostUpdater implements EventListenerInterface
 {
 
-	/**
-	 * ImplementedEvents method.
-	 *
-	 * @return array
-	 */
-	public function implementedEvents()
-	{
-		return [
-			'LastPostUpdater.new' => 'newPost',
-			'LastPostUpdater.delete' => 'deletePost'
-		];
-	}
+    /**
+     * ImplementedEvents method.
+     *
+     * @return array
+     */
+    public function implementedEvents()
+    {
+        return [
+            'LastPostUpdater.new' => 'newPost',
+            'LastPostUpdater.delete' => 'deletePost'
+        ];
+    }
 
-	public function __construct()
-	{
-		$this->ForumCategories = TableRegistry::get('ForumCategories');
-		$this->Threads = TableRegistry::get('ForumThreads');
-		$this->Posts = TableRegistry::get('ForumPosts');
-	}
+    public function __construct()
+    {
+        $this->ForumCategories = TableRegistry::get('ForumCategories');
+        $this->Threads = TableRegistry::get('ForumThreads');
+        $this->Posts = TableRegistry::get('ForumPosts');
+    }
 
-	/**
-	 * We send a new notification to an user.
-	 *
-	 * @param \Cake\Event\Event $event The event that was fired.
-	 *
-	 * @return false
-	 */
-	public function newPost(Event $event)
-	{
+    /**
+     * We send a new notification to an user.
+     *
+     * @param \Cake\Event\Event $event The event that was fired.
+     *
+     * @return false
+     */
+    public function newPost(Event $event)
+    {
 
-		if (!isset($event->data['thread']) || !isset($event->data['post'])) {
-			return false;
-		}
+        if (!isset($event->data['thread']) || !isset($event->data['post'])) {
+            return false;
+        }
 
-		if ($this->__updateCategories($event->data['thread'], $event->data['post'])) {
-			return true;
-		}
+        if ($this->__updateCategories($event->data['thread'], $event->data['post'])) {
+            return true;
+        }
 
-		return false;
+        return false;
 
-	}
+    }
 
-	public function deletePost(Event $event)
-	{
-		if (!isset($event->data['thread'])) {
-			return false;
-		}
-		$thread = $event->data['thread'];
+    /**
+     * @param Event $event
+     * @return bool
+     */
+    public function deletePost(Event $event)
+    {
+        if (!isset($event->data['thread'])) {
+            return false;
+        }
+        $thread = $event->data['thread'];
         $post = false;
 
-		if ($this->__updateCategories($thread, $post)) {
-			return true;
-		}
+        if ($this->__updateCategories($thread, $post)) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private function __updateCategories($thread, $post)
-	{
-		$category = $this->ForumCategories->get($thread->category_id);
-		$parents = $this->ForumCategories
-			->find()
-			->where([
-				'lft <=' => $category->lft,
-				'rght >=' => $category->rght
-			]);
-		foreach ($parents as $parent) {
+    /**
+     * Update Parents Categories
+     *
+     * @param $thread
+     * @param $post
+     * @return bool
+     */
+    private function __updateCategories($thread, $post)
+    {
+        $category = $this->ForumCategories->get($thread->category_id);
+        $parents = $this->ForumCategories
+            ->find()
+            ->where([
+                'lft <=' => $category->lft,
+                'rght >=' => $category->rght
+            ]);
+        foreach ($parents as $parent) {
             if (!$post) {
                 $conditions = [];
                 $childrens = $this->ForumCategories
                     ->find('children', ['for' => $parent->id])->toArray();
                 if ($childrens) {
-                    foreach($childrens as $child){
+                    foreach ($childrens as $child) {
                         $cdt = ['category_id' => $child->id];
-                        array_push( $conditions, $cdt);
+                        array_push($conditions, $cdt);
                     }
-                    $LastThread = $this->Threads->find()->where(['OR' => $conditions ])->order(['last_post_date' => 'DESC'])->first();
+                    $LastThread = $this->Threads->find()->where(['OR' => $conditions])->order(['last_post_date' => 'DESC'])->first();
                 } else {
-                    $LastThread = $this->Threads->find()->where(['category_id' => $parent->id ])->order(['last_post_date' => 'DESC'])->first();
+                    $LastThread = $this->Threads->find()->where(['category_id' => $parent->id])->order(['last_post_date' => 'DESC'])->first();
                 }
                 if ($LastThread) {
                     $parent->last_post_id = $LastThread->id;
@@ -103,7 +112,7 @@ class LastPostUpdater implements EventListenerInterface
                 $parent->last_post_id = $post->id;
                 $this->ForumCategories->save($parent);
             }
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 }
