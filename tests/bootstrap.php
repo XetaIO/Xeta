6d5450d7ee12aad2c7f7f73683ec1bf2f4b0f63f
Xeta <zoro.fmt@gmail.com>
2016-10-29 02:08:03 +0200

    test7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,7 @@ use Cake\Datasource\ConnectionManager;
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
@@ -18,3 +19,5 @@ if (!getenv('db_class')) {
     putenv('db_dsn=sqlite:///:memory:');
 }
 ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
+
+$_SERVER['PHP_SELF'] = '/';
