c0a66f5598bc4ad9fe6628a32e5ddf076050aeb6
Xety <emeric@xeta.io>
2017-04-23 17:08:14 +0200

    Prepare the 3.1.0 release

--- config/xeta.php
+++ config/xeta.php
@@ -10,7 +10,7 @@ return [
     ],
     'Site' => [
         'name' => 'Xeta',
-        'version' => '3.0.4',
+        'version' => '3.1.0',
         'description' => 'You will find content related to web development like tutorials, my personal tests on new technologies etc',
         'github_url' => 'https://github.com/XetaIO/Xeta',
         'contact_email' => 'contact@xeta.io',
